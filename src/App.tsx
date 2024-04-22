2d8d39d799c509899a855de0b89a2b889c795405
Keerty Verma <keertyverma1303@gmail.com>
2024-04-22 13:10:11 +0530

    Fetch recipes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,63 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+interface Recipe {
+  id: string;
+  title: string;
+  image: string;
+  time: number;
+  description: string;
+  vegan: boolean;
+}
+
 const App = () => {
+  const [recipes, setRecipes] = useState<Recipe[]>([]);
+
+  useEffect(() => {
+    const fetchRecipes = async () => {
+      try {
+        const response = await axios.get("http://localhost:3000/recipes");
+        setRecipes(response.data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    fetchRecipes();
+  }, []);
+
   return (
     <main>
       <nav>
         <h1>Recipes</h1>
       </nav>
+      <section className="grid grid-cols-3 gap-8">
+        {recipes.map((recipe) => (
+          <Card key={recipe.id}>
+            <CardHeader>
+              <div>
+                <CardTitle>{recipe.title}</CardTitle>
+                <CardDescription>{recipe.time} mins to cook.</CardDescription>
+              </div>
+            </CardHeader>
+            <CardContent>
+              <p>{recipe.description}</p>
+            </CardContent>
+            <CardFooter>
+              <button>View Recipe</button>
+              {recipe.vegan && <p>Vegan!</p>}
+            </CardFooter>
+          </Card>
+        ))}
+      </section>
     </main>
   );
 };
